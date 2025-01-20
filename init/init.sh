c8dc45fd9bfd945d06c819ce6715452ce2580821
Sivakov Vladislav <svv@salesap.ru>
2025-01-20 10:11:36 +0300

    upd

--- init/init.sh
+++
@@ -1,3 +0,0 @@
-echo "Распаковка dump.sql.gz и импорт в MySQL..."
-gunzip -c /docker-entrypoint-initdb.d/dump.sql.gz | mysql -u root -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE"
-echo "Импорт завершён."
