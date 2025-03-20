e3fd0cb26dc15d3d0de450b3b5dee26f2364119a
Sivakov Vladislav <svv@salesap.ru>
2025-03-20 10:47:14 +0300

    upd

--- server/php/push.php.sh
+++ server/php/push.php.sh
@@ -1,2 +1,3 @@
-scp php.ini root@217.114.9.212:/etc/php/8.3/fpm/php.ini
-ssh root@217.114.9.212 'systemctl restart php8.3-fpm'
+scp php.ini root@217.114.13.241:/etc/php/8.3/fpm/php.ini
+scp www.conf root@217.114.13.241:/etc/php/8.3/fpm/pool.d/www.conf
+ssh root@217.114.13.241 'systemctl restart php8.3-fpm'
